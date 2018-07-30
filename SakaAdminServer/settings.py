54f425f8fe03e95ab4ada3cdd0fa7d36ce06f01d
Timon Kosy <timonkosy92@gmail.com>
2018-07-30 22:14:21 +0300

    added media folder

--- SakaAdminServer/settings.py
+++ SakaAdminServer/settings.py
@@ -121,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL ='/media/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
--- SakaAdminServer/urls.py
+++ SakaAdminServer/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/serializers/',include('userAuthentication.serializers.urls')),
     path('property/serializers/',include('property_management.serializers.urls'))
 ]
+
+if settings.DEBUG:
+   urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+   urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
